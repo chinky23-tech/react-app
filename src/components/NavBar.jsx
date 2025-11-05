85afd2d2c34d812e404a0c74a925b00dd4836355
chinky23-tech <chinky0330@gmail.com>
2025-11-05 23:21:55 +0530

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,13 +20,22 @@ const NavBar = () => {
           <div className="flex space-x-3">
         {/*<button className="bg-transparent text-white py-1 px-3 rounded border-2">Login</button>
           <button className="bg-green-900 text-white py-1 px-3 rounded border-2">SignUp</button>*/}
-   <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium  rounded-lg  bg-linear-to-br from-teal-400 to-lime-100 text-white  focus:outline-none">
+   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 
+   overflow-hidden text-sm font-medium  rounded-lg  bg-linear-to-br from-teal-400 to-lime-100 text-white 
+    focus:outline-none">
 <span class="relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-orange-600 rounded-md ">
 Log In
 </span>
-</button>
+</button>  
+ <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 
+   overflow-hidden text-sm font-medium  rounded-lg  bg-linear-to-br from-teal-400 to-lime-100 text-white 
+    focus:outline-none">
+<span class="relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-teal-600 rounded-md ">
+Sign In
+</span>
+</button>  
 
-           </div>
+  </div>
 
 
 
