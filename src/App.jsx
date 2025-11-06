8997b5f8de865ca2d608c5a82f21db9d5b4232ba
chinky23-tech <chinky0330@gmail.com>
2025-11-06 22:41:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -35,49 +35,18 @@ function App() {
 export default App*/
 
 import { useState, useEffect } from "react";
-import SearchBar from "./components/SearchBar";
-import RecipeCard from "./components/RecipeCard";
+
 import NavBar  from "./components/NavBar";
-import Home from "./pages/Home.jsx";
+
 
 function App() {
-
-  const [recipes, setRecipes] = useState([]);
-  const [query, setQuery] = useState("chicken");
-
-  useEffect(() => {
-    fetchRecipes();
-  }, [query]);
-
-  const fetchRecipes = async () => {
-    try {
-      const res = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
-      );
-      const data = await res.json();
-      setRecipes(data.meals || []);
-    } catch (error) {
-      console.error("Error fetching recipes:", error);
-    }
-  };
-
-  return (
+return(
   <>
   <NavBar />
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-white m-14 transition-all">
-      <h1 className="text-4xl font-bold text-center pt-8">🍳 Recipe Finder</h1>
-      <SearchBar setQuery={setQuery} />
-      <div className="grid gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {recipes.length > 0 ? (
-          recipes.map((meal) => <RecipeCard key={meal.idMeal} meal={meal} />)
-        ) : (
-          <p className="text-center col-span-full text-gray-500">No recipes found 😔</p>
-        )}
-      </div>
-    </div>
+  
    </>
- 
-  );
+);
+
 }
 
 export default App;
